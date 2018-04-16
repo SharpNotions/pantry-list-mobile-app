4ed9ab56632243d21276da4a3920363390f8a2d6
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2018-04-16 08:56:03 -0400

    Remove dead code

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,24 +12,6 @@ export const getUserInfo = async accessToken => {
   return userInfoResponse;
 };
 
-// export const signInWithGoogleAsync = () => {
-//   try {
-//     const result = await Expo.Google.logInAsync({
-//       androidClientId: '430381753851-umurbastd4s7rj2mli11fjo9tkfk43a5.apps.googleusercontent.com',
-//       iosClientId: '430381753851-4i8ed01u344vd89l61d561jekcprpmqu.apps.googleusercontent.com',
-//       scopes: ['profile', 'email'],
-//     });
-//
-//     if (result.type === 'success') {
-//       return result.accessToken;
-//     } else {
-//       return {cancelled: true};
-//     }
-//   } catch(e) {
-//     return {error: true};
-//   }
-// }
-
 export const fetchList = async () => {
   const response = await fetch(Config.apiUrl);
   const listData = await response.json();
