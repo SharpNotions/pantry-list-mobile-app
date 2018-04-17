3576f4fae7892eb138b75c18886ce9378dc55c13
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2018-04-17 17:34:29 -0400

    update example

--- config/index.example.js
+++ config/index.example.js
@@ -1,5 +1,6 @@
 export const Config = {
   androidClientId: '',
   iosClientId: '',
-  apiUrl: 'https://localhost:4000'
+  apiUrl: 'https://localhost:4000',
+  barcodeAPI: 'https://api.upcitemdb.com/prod/trial/lookup?upc='
 };
